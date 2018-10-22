6c132138cf3fdc1f6655f22c63aa4b295170cd10
zhaoYun-Long <17638562956@163.com>
2018-10-22 16:15:51 +0800

    改为jsx形式

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react' // 创建组件，虚拟dom元素，生命周期
 import ReactDOM from 'react-dom' // 把创建好的组件和虚拟dom放到页面上展示
 
-const myh1 = React.createElement('h1', null, 'this is h1 ..')
+// const myh1 = React.createElement('h1', null, 'this is h1 ..')
+const div = <div>6666</div>
 ReactDOM.render(myh1, document.querySelector('#app'))
