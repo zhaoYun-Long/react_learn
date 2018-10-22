5e979c01365e6fa824d74342e703d489217ea035
zhaoYun-Long <17638562956@163.com>
2018-10-22 16:19:26 +0800

    222

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react' // 创建组件，虚拟dom元素，生命周期
 import ReactDOM from 'react-dom' // 把创建好的组件和虚拟dom放到页面上展示
 
 // const myh1 = React.createElement('h1', null, 'this is h1 ..')
-const div = <div>6666</div>
+const div = <div><span>777</span></div>
 ReactDOM.render(myh1, document.querySelector('#app'))
